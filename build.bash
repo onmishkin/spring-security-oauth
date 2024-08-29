03fe14006aa6870e7083135d8e54a2b3e81471dc
Nat Mishkin <nmishkin@ptc.com>
2024-08-29 17:40:14 -0400

    mvn command wrapper

---
+++ build.bash
@@ -0,0 +1,9 @@
+# Add "-DskipTests=true" to skip tests
+# Add "--fail-at-end" to continue through failures
+# Add "-e" to get stack trace
+
+#JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home
+JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-17.jdk/Contents/Home
+
+# "-Dmaven.javadoc.skip=true" needed for Java 17
+mvn -Dmaven.javadoc.skip=true package
